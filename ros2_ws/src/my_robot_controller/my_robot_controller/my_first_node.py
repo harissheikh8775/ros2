fba20afc564de5fd8a55ad4a717dc730829a88c0
harissheikh8775 <harisbadboi@gmail.com>
2024-11-03 00:04:58 +0530

    first ros2 node

--- ros2_ws/src/my_robot_controller/my_robot_controller/my_first_node.py
+++ ros2_ws/src/my_robot_controller/my_robot_controller/my_first_node.py
@@ -6,6 +6,34 @@ class MyNode(Node):
 
     def __init__(self):  # Constructor
         super().__init__("first_node")
+        self.create_timer(1.0,self.timer_callback)
+
+
+        def timer_callback(self):
+            self.get_logger().info("Hello")
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyNode()  # Create an instance of MyNode
+    rclpy.spin(node)  # Keep the node running until manually interrupted
+    node.destroy_node()  # Clean up the node
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+
+'''
+***************************first node program**********************************
+
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+class MyNode(Node):
+
+    def __init__(self):  # Constructor
+        super().__init__("first_node")
         self.get_logger().info("ROS2!")
 
 def main(args=None):
@@ -17,3 +45,5 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
+'''
