4583c650da929ab62768e2f7e5ed6ada1b3828b2
harissheikh8775 <harisbadboi@gmail.com>
2024-11-03 00:07:05 +0530

    first ros2 node

--- ros2_ws/src/my_robot_controller/my_robot_controller/my_first_node.py
+++ ros2_ws/src/my_robot_controller/my_robot_controller/my_first_node.py
@@ -8,9 +8,8 @@ class MyNode(Node):
         super().__init__("first_node")
         self.create_timer(1.0,self.timer_callback)
 
-
-        def timer_callback(self):
-            self.get_logger().info("Hello")
+    def timer_callback(self):
+        self.get_logger().info("Hello")
 
 def main(args=None):
     rclpy.init(args=args)
